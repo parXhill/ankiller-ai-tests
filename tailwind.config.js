40b21cd816b75333e542d1b78918b2a77b2e9a8d
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-11-22 16:50:56 +1100

    Return basic response in parseable JSON

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust this if using the src directory structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
